6e212e64fe356755bf11736a0e364496b6784e10
e-mustafa <eng.mustafax@gmail.com>
2026-06-27 20:30:02 +0300

    fix static path

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,5 +1,7 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { configEnv } from './config/env.js';
 import connectDB from './DB/connection.js';
 import { authRouter, authRoutes, userRouter, userRoutes } from './modules/index.js';
@@ -8,9 +10,12 @@ import { connectRedis } from './utils/redis/client.redis.js';
 import { successResponse } from './utils/response/success.response.js';
 import { NotFoundException } from './utils/response/throw.exceptions.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default async function bootstrap(app, express) {
 	// middlewares
-	app.use(cors({ origin: '*' })); // for handling CORS
+	app.use(cors({ origin: configEnv.frontendUrl, credentials: true })); // for handling CORS
 	app.use(express.json({ limit: '2mb' })); // for parsing JSON bodies
 	app.use(cookieParser()); // for parsing cookies
 
@@ -23,7 +28,7 @@ export default async function bootstrap(app, express) {
 	});
 
 	// routers
-	app.use('/uploads', express.static('./uploads'));
+	app.use('/uploads', express.static(path.join(__dirname, './uploads')));
 	app.use(configEnv.urlApiBase + authRoutes.base, authRouter);
 	app.use(configEnv.urlApiBase + userRoutes.base, userRouter);
 
