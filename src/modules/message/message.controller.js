d8bec21596a7e0b151994494dfe859c121b79ec8
e-mustafa <eng.mustafax@gmail.com>
2026-07-05 18:56:12 +0300

    enhance - messages change serializeMessage function to class and use it in controller

--- src/modules/message/message.controller.js
+++ src/modules/message/message.controller.js
@@ -1,23 +1,30 @@
 import asyncHandler from '../../utils/error-handler/async-handler.js';
 import { successResponse } from '../../utils/response/success.response.js';
 import { deleteMessageService, getMessageService, getMessagesService, sendMessageService } from './message.services.js';
+import { MessageDTO } from './message.utils.js';
 
 // @desc    Send an anonymous message
 // @route   POST /messages/:username
 export const sendMessages = asyncHandler(async (req, res) => {
 	const { user, body, params, files } = req || {};
-	const data = await sendMessageService({
+	const messages = await sendMessageService({
 		userId: user._id || null,
 		username: params.username,
 		files,
 		...body,
 	});
+
+	// format message and remove from field (sender) if message is anonymous
+	const data = MessageDTO.single(messages);
 	successResponse({ res, message: 'Message sent successfully', data });
 });
 
 export const getMessages = asyncHandler(async (req, res) => {
 	const { user } = req || {};
 	const data = await getMessagesService(user._id);
+
+	// format messages and remove from field (sender) if message is anonymous
+	data.data = MessageDTO.list(data.data, !!user.role);
 	successResponse({ res, message: 'Messages retrieved successfully', data });
 });
 
@@ -26,19 +33,22 @@ export const getMessagesType = (type) =>
 	asyncHandler(async (req, res) => {
 		const { user, query } = req || {};
 		const data = await getMessagesService(user._id, type, query);
+
+		// format messages and remove from field (sender) if message is anonymous
+		data.data = MessageDTO.list(data.data, !!user.role);
 		successResponse({ res, data });
 	});
 // get single message by id
 export const getMessage = asyncHandler(async (req, res) => {
 	const { user, params } = req || {};
-	const data = await getMessageService(user._id, params.id || '');
+	const message = await getMessageService(user._id, params.id || '');
+	const data = MessageDTO.single(message, !!user.role);
 	successResponse({ res, data });
 });
 
 // delete message by id
 export const deleteMessage = asyncHandler(async (req, res) => {
 	const { user, params } = req || {};
-	console.log(user);
 	await deleteMessageService(user._id, params.id || '');
 	successResponse({ res, message: 'Message deleted successfully' });
 });
--- src/modules/message/message.utils.js
+++ src/modules/message/message.utils.js
@@ -1,22 +1,79 @@
 import { configEnv } from '../../config/env.js';
+import { decrypt } from '../../utils/security/encryption.security.js';
 
 // format message and remove from field (sender) if message is anonymous
-export function serializeMessage(message) {
-	if (message.attachments && message.attachments?.length > 0) {
-		message.attachments = message.attachments.map((attachment) => {
-			return {
-				url: `${configEnv.appUrl}/${attachment.url.replace(/\\/g, '/')}`,
-				fileType: attachment.fileType,
+// export function serializeMessage(message) {
+// 	if (message.attachments && message.attachments?.length > 0) {
+// 		message.attachments = message.attachments.map((attachment) => {
+// 			return {
+// 				url: `${configEnv.appUrl}/${attachment.url.replace(/\\/g, '/')}`,
+// 				fileType: attachment.fileType,
+// 			};
+// 		});
+// 	}
+
+// 	if (message.isConfidential) {
+// 		message.content = decrypt(message.content);
+// 	}
+
+// 	return {
+// 		id: message._id,
+// 		content: message.content,
+// 		attachments: message.attachments,
+// 		createdAt: message.createdAt,
+// 		isFavorite: message.isFavorite,
+// 		from: message.isAnonymous ? null : message.from,
+// 		to: message.to ? message.to : null,
+// 	};
+// }
+
+export class MessageDTO {
+	static single(message, isAdmin = false) {
+		// remove from (sender) data if message is anonymous
+		if (message.isAnonymous) {
+			message.from = null;
+		}
+
+		// decrypt content if message is confidential
+		if (message?.isConfidential && message?.content) {
+			// message.content = '🔒 This message is confidential';
+			message.content = decrypt(message.content);
+		}
+
+		if (message.attachments && message.attachments?.length > 0) {
+			message.attachments = message.attachments.map((attachment) => {
+				return {
+					url: `${configEnv.appUrl}/${attachment.url.replace(/\\/g, '/')}`,
+					fileType: attachment.fileType,
+				};
+			});
+		}
+
+		if (!message.isAnonymous && message.from) {
+			message.from = {
+				id: message.from._id,
+				name: `${message.from.firstName} ${message.from.lastName}`,
+				avatar: message.from.profileImage
+					? `${configEnv.appUrl}/${message.from.profileImage.replace(/\\/g, '/')}`
+					: null,
 			};
-		});
+		}
+
+		// return data;
+		return {
+			id: message._id,
+			content: message.content,
+			attachments: message.attachments,
+			createdAt: message.createdAt,
+			isFavorite: message.isFavorite,
+			from: !isAdmin && message.isAnonymous ? null : message.from,
+			to: message.to ? message.to : null,
+		};
 	}
-	return {
-		id: message._id,
-		content: message.content,
-		attachments: message.attachments,
-		createdAt: message.createdAt,
-		isFavorite: message.isFavorite,
-		from: message.isAnonymous ? null : message.from,
-		to: message.to ? message.to : null,
+
+	static list = function (messages, isAdmin = false) {
+		// console.log('messages', messages);
+
+		return messages.map((message) => this.single(message, isAdmin));
 	};
 }
