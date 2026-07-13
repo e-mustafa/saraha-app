d92f07fe7bf1f56f28e8dba57329b633b0d1cde1
e-mustafa <eng.mustafax@gmail.com>
2026-07-14 00:59:10 +0300

    use  vercel function waitUntil to avoid loss emails

--- src/utils/emails/sendEmail.js
+++ src/utils/emails/sendEmail.js
@@ -1,6 +1,6 @@
+import { waitUntil } from '@vercel/functions';
 import nodemailer from 'nodemailer';
 import { configEnv, emailConfig } from '../../config/env.js';
-import asyncHandler from '../error-handler/async-handler.js';
 
 const transporter = nodemailer.createTransport({
 	// host: emailConfig.smtp.host,
@@ -13,14 +13,44 @@ const transporter = nodemailer.createTransport({
 	},
 });
 
-export const sendEmail = asyncHandler(async ({ to, subject, html }) => {
-	const info = await transporter.sendMail({
-		from: `${configEnv.appName} <${emailConfig.googleEmail}>`,
-		to,
-		subject,
-		html,
-	});
+// export const sendEmail = asyncHandler(async ({ to, subject, html }) => {
+// 	const info = await transporter.sendMail({
+// 		from: `${configEnv.appName} <${emailConfig.googleEmail}>`,
+// 		to,
+// 		subject,
+// 		html,
+// 	});
 
-	console.log(info);
-	return info;
-});
+// 	console.log(info);
+// 	return info;
+// });
+
+export const sendEmail = async ({ to, subject, html }) => {
+	// create promise for sending email
+	const mailPromise = transporter
+		.sendMail({
+			from: `${configEnv.appName} <${emailConfig.googleEmail}>`,
+			to,
+			subject,
+			html,
+		})
+		.then((info) => {
+			console.log('✔ Email sent successfully:', info.messageId);
+			return info;
+		})
+		.catch((err) => {
+			console.error('❌ Email sending failed:', err.message);
+		});
+
+	// pass the promise to Vercel to prevent server freezing (using vercel function waitUntil)
+	try {
+		waitUntil(mailPromise);
+	} catch (e) {
+		// this part is for safety: if you are running the app locally (Local) without Vercel environment
+		// the waitUntil function will not be available, so we will let the Event Loop handle the Promise naturally without blocking
+		console.log('💡 Running outside Vercel. Email sent in local background.', e);
+	}
+
+	// return immediate response to the Controller that the process has started in the background
+	return { status: 'processing' };
+};
