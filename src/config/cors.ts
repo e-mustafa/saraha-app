03e1ea392bc2229e3ae759e4cb4fc7048771cd9b
e-mustafa <eng.mustafax@gmail.com>
2026-07-15 02:30:53 +0300

    fix security file issue

--- src/config/cors.ts
+++
@@ -1,6 +0,0 @@
-import { configEnv } from './env.js';
-
-export const corsOptions = {
-	origin: [configEnv.frontendUrl, ...(configEnv.allowedOrigin?.split(',') || [])],
-	credentials: true,
-};
---
+++ src/config/security.config.js
@@ -0,0 +1,26 @@
+import RedisStore from 'rate-limit-redis';
+import { redisDB } from '../utils/redis/client.redis.js';
+import { configEnv } from './env.js';
+
+// CORS Configuration --------------------------------------------------------
+const allowedOrigins = [configEnv.frontendUrl, ...(configEnv.allowedOrigin ? configEnv.allowedOrigin.split(',') : [])]
+	.map((origin) => origin?.trim()) // Remove any accidental leading/trailing spaces
+	.filter(Boolean); // Filter out undefined, null, or empty strings
+
+export const corsOptions = {
+	origin: allowedOrigins,
+	credentials: true,
+};
+
+// Rate Limit Configuration --------------------------------------------------------
+export const rateLimitConfig = {
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // limit each IP to 100 requests per windowMs
+	standardHeaders: false,
+	legacyHeaders: false,
+	statusCode: 429,
+	message: 'Too many requests, please try again later.',
+	store: new RedisStore({
+		sendCommand: (...args) => redisDB.sendCommand(args),
+	}),
+};
